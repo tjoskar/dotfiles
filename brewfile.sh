4bca5bddc9349a5ffc649f4c44829c69c63e3132
tjoskar <kontakta@oskarkarlsson.nu>
2014-10-18 09:19:22 +0200

    Fixed #10

--- brewfile.sh
+++ brewfile.sh
@@ -1,29 +1,29 @@
 # Upgrade any already-installed formulae
-upgrade
+brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated)
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-install coreutils
+brew install coreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-install findutils
+brew install findutils
 # Install Bash 4
-install bash
+brew install bash
 
 # Install more recent versions of some OS X tools
-install vim --override-system-vi
-tap homebrew/dupes
-install homebrew/dupes/grep
-tap josegonzalez/homebrew-php
-install php55
-install python
+brew install vim --override-system-vi
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew tap josegonzalez/homebrew-php
+brew install php55
+brew install python
 
 # Install other useful binaries
-install git
-install node
-install tree
-install p7zip
-install trash
-install phantomjs
+brew install git
+brew install node
+brew install tree
+brew install p7zip
+brew install trash
+brew install phantomjs
 
 # Remove outdated versions from the cellar
-cleanup
+brew cleanup

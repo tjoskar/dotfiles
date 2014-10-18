be2d0d642de6b90f6e1b40622c5fe897d9aebb54
tjoskar <kontakta@oskarkarlsson.nu>
2014-10-18 11:46:52 +0200

    Fixed #7

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 if command -v brew > /dev/null 2>&1; then # Homebrew installed
     brew update
 else
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 sorce brewfile.sh
